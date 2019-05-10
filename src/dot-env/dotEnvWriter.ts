8ed1e3e6ca447b0b2bb64b15f50f8cb3d8825932
Tomi Laurell <tomi.laurell@gmail.com>
2019-05-10 18:25:41 +0300

    Fix: dot-env files were missing first value

--- src/dot-env/dotEnvWriter.ts
+++ src/dot-env/dotEnvWriter.ts
@@ -4,8 +4,9 @@ export function writeDotEnvFile(path: string, properties: any): Promise<void> {
   const fileName = `${path}/.env`;
   console.log(`Generating file: '${fileName}'`);
 
-  const content = Object.keys(properties).reduce((value: string = "", key: string) => `${value}\n${key}=${properties[key]}`);
-  console.log("Writing content", content);
+  const content = Object.keys(properties)
+    .map((key: string) => `${key}=${properties[key]}`)
+    .reduce((v: string = "", n: string) => `${v}\n${n}`);
 
   return new Promise(function(resolve, reject) {
     fs.writeFile(fileName, content, err => {
